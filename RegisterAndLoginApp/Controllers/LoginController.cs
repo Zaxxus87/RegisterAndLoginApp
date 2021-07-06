619788a33ff2b89162ae80f4f75d9d2e7f12fb3b
Jeffrey Baker <jeffrey1871@gmail.com>
2021-07-06 11:04:53 -0500

    initial commit

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+using RegisterAndLoginApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.IsValid(userModel))
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+            
+        }
+    }
+}
